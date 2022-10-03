85eced9aa4821e0feec011f3f85300dbc09ed5e7
Damo <damodharan.jayachandran@zohocorp.com>
2022-10-03 18:49:18 +0530

    Update UX, navigations & validations

--- ui/src/App.js
+++ ui/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import { SqlEditor } from './components/sql-editor/sql-editor';
 import { generateMetaData, generateID, createEditor, generateRowData } from './utils';
 import { DataEditor, GridCellKind  } from '@glideapps/glide-data-grid';
+import { ToastContainer, toast } from 'react-toastify';
 
 import "@glideapps/glide-data-grid/dist/index.css";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -28,6 +30,14 @@ function App() {
     }
   }
 
+  const duplicateValidation = (newValue) =>
+    editors.reduce((acc, x, i) => {
+      if (x.query === newValue.trim()) {
+        return i;
+      }
+      return acc;
+    }, -1);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -35,16 +45,44 @@ function App() {
           <div style={{display: 'flex', flexDirection: 'row'}}>
             <div className="sql-list">
               <SqlEditor createOnly={true} onCreate={(newValue) => {
+                newValue = newValue.trim();
+                const index = duplicateValidation(newValue);
+                if (index > -1) {
+                  toast.info('Query already present, navigated')
+                  setSelectedEditor(index);
+                  return;
+                }
+                toast.info('Created a new query')
                 setEditors([createEditor(newValue), ...editors])
                 setSelectedEditor(0);
               }}></SqlEditor>
+              <div class="sql-editors">
               {editors.map((x, i) => <SqlEditor 
                 createOnly={false}
+                onChange={(query, setValue) => {
+                  const index = duplicateValidation(query);
+                  if (index === -1) {
+                    setEditors([
+                      ...editors.slice(0, i),
+                      {...editors[selectedEditor], query},
+                      ...editors.slice(i + 1, editors.length)
+                    ])
+                    setValue(query);
+                    toast.info('Updated query');
+                  } else {
+                    toast.error('Query already present, navigated & resetting')
+                    setSelectedEditor(index);
+                    setTimeout(() => {
+                      setValue(editors[selectedEditor].query);
+                    }, 2000);
+                  }
+                }}
                 onDelete={() => {
                   setEditors([
                     ...editors.slice(0, i),
                     ...editors.slice(i + 1, editors.length)
                   ])
+                  toast.info('Deleted query')
                 }}
                 onExecute={(reExecute = false) => {
                   if (reExecute) {
@@ -53,9 +91,7 @@ function App() {
                       {...editors[selectedEditor], data: generateRowData(500, editors[selectedEditor].metaInfo.length)},
                       ...editors.slice(i + 1, editors.length)
                     ])
-                    console.log(
-                      {...editors[selectedEditor], data: generateRowData(500, editors[selectedEditor].metaInfo.length)}
-                    );
+                    toast.info('Re-executed query')
                   }
                   setSelectedEditor(i);
                 }}
@@ -64,12 +100,14 @@ function App() {
                 key={x.id} 
                 readOnly={false}
                 ></SqlEditor>)}
+              </div>
             </div>
             <div className="data-grid">
               {editors[selectedEditor] ? <DataEditor height="100%" getCellContent={generateData} columns={editors[selectedEditor].metaInfo} rows={100}/> : <></>}
             </div>
           </div>
       </header>
+      <ToastContainer autoClose={500}></ToastContainer>
     </div>
   );
 }
--- ui/src/components/sql-editor/sql-editor.js
+++ ui/src/components/sql-editor/sql-editor.js
@@ -1,24 +1,38 @@
-
-import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useEffect, useRef, useState } from 'react';
 import './sql-editor.css';
 
-export function SqlEditor({value, selected=false, placeholder="Enter your SQL", createOnly = false, readOnly, onCreate = () => {}, onEdit = () => {}, onDelete = () => {}, onExecute = () => {}}) {
+export function SqlEditor({value, selected=false, placeholder="Enter your SQL", createOnly = false, readOnly, onCreate = () => {}, onEdit = () => {}, onDelete = () => {}, onExecute = () => {},  onChange = () => {}}) {
 
     const [inpValue, setInpValue] = useState(value)
-    return <div className={selected ? 'sql-editor selected' : 'sql-editor'} onClick={(e) => {
+
+    const ref = useRef();
+
+    useEffect(() => {
+        if(selected) {
+            ref.current.scrollIntoView({behavior: 'smooth'});
+        }
+    });
+
+    return <div ref={ref} className={selected ? 'sql-editor selected' : 'sql-editor'} onClick={(e) => {
         if (e.target.classList.contains('button')) {
             return;
         }
         onExecute();
     }} >
         <textarea placeholder={placeholder} readOnly={readOnly} className="editor" value={inpValue} onChange={(e) => {
-            setInpValue(e.target.value);
+            if (createOnly) {
+                setInpValue(e.target.value);
+            }
+            onChange.bind(this, e.target.value, setInpValue)();
         }}></textarea>
         <div className="bottom-actions">
             {createOnly ? 
-            <button className="execute-button" onClick={
+            <button className="execute-button" onClick={ () => {
                 inpValue ?
-                onCreate.bind(this, inpValue) : () => { }
+                onCreate.bind(this, inpValue)() :
+                toast.error('Invalid Query');
+            }
             }>Create</button> :
             <>
                 <button className="button" onClick={onExecute.bind(this, true)}>Re-Execute</button>
